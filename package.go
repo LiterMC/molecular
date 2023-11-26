9025ae1b2920a91adc73446ade20bd2fe4f8c119
zyxkad <zyxkad@gmail.com>
2023-11-25 17:46:33 -0700

    add README.MD

--- package.go
+++ package.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+// molecule is a 3D physics engine that designed for realistic gravity
+//
 // The default units used by this package:
 // Distance and Position: m (meter)
 // Time: s (second)
